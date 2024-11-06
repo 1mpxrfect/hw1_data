8800e89ee43f5f6df67a50fd35ca0b5c702d30a0
nurs <zh.nursultan27@gmail.com>
2024-11-06 08:34:04 +0500

    aa

--- Task2/task_2.py
+++ Task2/task_2.py
@@ -12,3 +12,20 @@ def calculate_aggregated_threat_score(departments: List[Dict[str, any]]) -> floa
     aggregated_score = sum(weighted_scores) / total_importance if total_importance else 0
     return min(max(aggregated_score, 0), 90)
 
+
+if __name__ == '__main__':
+    mean = 50
+    variance = 10
+    num_samples = 5
+
+    scores = generate_random_data(mean, variance, num_samples)
+    print("Random data: ", scores)
+
+    departments = [
+        {'scores': generate_random_data(60, 15, 5), 'importance': 2},
+        {'scores': generate_random_data(40, 10, 5), 'importance': 1},
+        {'scores': generate_random_data(30, 5, 5), 'importance': 3}
+    ]
+
+    aggregated_score = calculate_aggregated_threat_score(departments)
+    print("Rate security: ", aggregated_score)
