79f8c0f95816ade66cdfb1837a58b527051cd802
nrsltnzhmnv <96646919+1mpxrfect@users.noreply.github.com>
2024-11-26 12:23:37 +0500

    Update utils.py

--- Task3/utils.py
+++ Task3/utils.py
@@ -1,5 +1,13 @@
+import math
+
 def time_difference(hour1, hour2):
-    diff = abs(hour2 - hour1)
-    if diff > 12:
-        diff = 24 - diff
-    return diff
+    sin1, cos1 = math.sin(2 * math.pi * hour1 / 24), math.cos(2 * math.pi * hour1 / 24)
+    sin2, cos2 = math.sin(2 * math.pi * hour2 / 24), math.cos(2 * math.pi * hour2 / 24)
+    diff_sin = sin2 - sin1
+    diff_cos = cos2 - cos1
+    diff = math.sqrt(diff_sin**2 + diff_cos**2)
+    angle_diff = math.acos(diff)
+    diff_hours = angle_diff * 24 / (2 * math.pi)
+    if diff_hours > 12:
+        diff_hours = 24 - diff_hours
+    return diff_hours
