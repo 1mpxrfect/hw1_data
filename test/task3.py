b14d12ff47cd0154c41159fb971d5d84cf03d912
nrsltnzhmnv <96646919+1mpxrfect@users.noreply.github.com>
2024-11-26 12:27:35 +0500

    Update task3.py

--- test/task3.py
+++ test/task3.py
@@ -1,7 +1,6 @@
 import unittest
 from Task3.utils import time_difference
 
-
 class TestTimeDifference(unittest.TestCase):
 
     def test_same_time(self):
@@ -23,6 +22,51 @@ class TestTimeDifference(unittest.TestCase):
         self.assertEqual(time_difference(1, 23), 2)
         self.assertEqual(time_difference(23, 1), 2)
 
+    def test_edge_case_noon_midnight(self):
+        self.assertEqual(time_difference(12, 0), 12)
+        self.assertEqual(time_difference(0, 12), 12)
+
+    def test_wraparound(self):
+        self.assertEqual(time_difference(6, 30), 6)
+        self.assertEqual(time_difference(30, 6), 6)
+
+    def test_large_time_difference(self):
+        self.assertEqual(time_difference(4, 18), 10)
+        self.assertEqual(time_difference(18, 4), 10)
+
+    def test_exactly_halfway(self):
+        self.assertEqual(time_difference(0, 12), 12)
+        self.assertEqual(time_difference(12, 0), 12)
+
+    def test_time_just_before_midnight(self):
+        self.assertEqual(time_difference(23, 0), 1)
+        self.assertEqual(time_difference(0, 23), 1)
+
+    def test_cyclic_case_larger_than_24(self):
+        self.assertEqual(time_difference(25, 7), 6)
+        self.assertEqual(time_difference(7, 25), 6)
+
+    def test_case_around_6AM(self):
+        self.assertEqual(time_difference(6, 5), 1)
+        self.assertEqual(time_difference(5, 6), 1)
+
+    def test_case_around_6PM(self):
+        self.assertEqual(time_difference(18, 19), 1)
+        self.assertEqual(time_difference(19, 18), 1)
+
+    def test_crossing_from_23_to_1(self):
+        self.assertEqual(time_difference(23, 1), 2)
+        self.assertEqual(time_difference(1, 23), 2)
+
+    def test_case_before_and_after_3AM(self):
+        self.assertEqual(time_difference(2, 3), 1)
+        self.assertEqual(time_difference(3, 2), 1)
+        self.assertEqual(time_difference(0, 3), 3)
+        self.assertEqual(time_difference(3, 0), 3)
+
+    def test_time_difference_equal_to_0AM(self):
+        self.assertEqual(time_difference(0, 23), 1)
+        self.assertEqual(time_difference(23, 0), 1)
 
 if __name__ == '__main__':
     unittest.main()
